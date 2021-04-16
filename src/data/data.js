384d16ece285236a777f48f63998bbe35baf5cae
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-04-16 17:58:00 +0530

    create : data file containing curated videos data

---
+++ src/data/data.js
@@ -0,0 +1,335 @@
+import { v4 as uuidv4 } from "uuid";
+
+const videoLib = [
+  {
+    id: "gnscqweM_NU",
+    image: "http://i3.ytimg.com/vi/gnscqweM_NU/maxresdefault.jpg",
+    name: "Figma Tutorial: Shape Tools",
+    description:
+      "Learn How to create shapes in Figma, Selecting and resizing shapes using keyboard shortcuts, How to find and just shape properties, How to change shape fills",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "5x2uHUB_pzw",
+    image: "http://i3.ytimg.com/vi/5x2uHUB_pzw/maxresdefault.jpg",
+    name: "Figma Tutorial: Pen Tool Basics & Vector Networks",
+    description:
+      "In this video we'll learn how to use the pen tool and vector networks.",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "NrKX46DzkGQ",
+    image: "http://i3.ytimg.com/vi/NrKX46DzkGQ/maxresdefault.jpg",
+    name: "Figma Tutorial: Auto Layout (older version)",
+    description:
+      "With Auto Layout, buttons can resize with their text, lists can be rearranged by simply dragging items around, and your Frames will grow or shrink as you add or remove content. ",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "LHY9cm_2zwU",
+    image: "http://i3.ytimg.com/vi/LHY9cm_2zwU/maxresdefault.jpg",
+    name: "Figma Tutorial: Constraints",
+    description: "Learn how you can leverage constraints in figma.",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "gUb303HQSGk",
+    image: "http://i3.ytimg.com/vi/gUb303HQSGk/maxresdefault.jpg",
+    name: "Figma Tutorial: Gradients (Linear, Radial, Angular, Diamond)",
+    description:
+      "In this video we'll learn how to use various types of gradients in our designs.",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "e1BRH09iwes",
+    image: "http://i3.ytimg.com/vi/e1BRH09iwes/maxresdefault.jpg",
+    name: "Figma Tutorial: Effects",
+    description:
+      "In this video we'll show you how to change the appearance of objects on your canvas by using Effects. ",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "V7waqacFYZs",
+    image: "http://i3.ytimg.com/vi/V7waqacFYZs/maxresdefault.jpg",
+    name: "Figma Tutorial: Color Picker",
+    description:
+      "In this video we'll talk about the Color Picker and how you can select and save colors in Figma.",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "gtQ_A3imzsg",
+    image: "http://i3.ytimg.com/vi/gtQ_A3imzsg/maxresdefault.jpg",
+    name: "Figma Tutorial: Creating Styles",
+    description:
+      "In this video we'll show you how to create Styles and use them across your designs.",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "L7dtFGUd-C0",
+    image: "http://i3.ytimg.com/vi/L7dtFGUd-C0/maxresdefault.jpg",
+    name: "Figma Tutorial: Rounded Corners",
+    description:
+      "In this video we'll show you how to soften the corners of rectangles, stars, or other polygons with Figma's corner rounding feature.",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "5i-ebNTjad8",
+    image: "http://i3.ytimg.com/vi/5i-ebNTjad8/maxresdefault.jpg",
+    name: "Figma Tutorial: Text Tool and Fonts",
+    description:
+      "In this video we’ll cover the basics of using the text tool and fonts..",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "Dn0Ldi1SPv0",
+    image: "http://i3.ytimg.com/vi/Dn0Ldi1SPv0/maxresdefault.jpg",
+    name: "Figma Tutorial: Masks",
+    description:
+      "In this video we'll learn how Masks can be used to show, and hide, specific portions of other layers on your canvas.",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "8ZaX6qEcUCU",
+    image: "http://i3.ytimg.com/vi/8ZaX6qEcUCU/maxresdefault.jpg",
+    name: "Figma Tutorial: Boolean Operations",
+    description:
+      "In this video we'll learn how to combine shapes using each of the four Boolean Operations: Union, Subtract, Intersect, and Exclude.",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "saoB8uqUAH8",
+    image: "http://i3.ytimg.com/vi/saoB8uqUAH8/maxresdefault.jpg",
+    name: "Figma Tutorial: Images",
+    description:
+      "In this video we'll show you how to add and use images in your designs. ",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "qQyaPqem0TA",
+    image: "http://i3.ytimg.com/vi/qQyaPqem0TA/maxresdefault.jpg",
+    name: "Figma Tutorial: Alignment & Distribution",
+    description:
+      "In this video we'll learn how to align and distribute objects on our canvas.",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "zd8wrAdURN0",
+    image: "http://i3.ytimg.com/vi/zd8wrAdURN0/maxresdefault.jpg",
+    name: "Figma Tutorial: Layout Grids",
+    description:
+      "In this video we'll discuss the various types of Layout Grids you can use in Figma when working on responsive designs. ",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "0xafhHNEsp8",
+    image: "http://i3.ytimg.com/vi/0xafhHNEsp8/maxresdefault.jpg",
+    name: "Figma Tutorial: Guides",
+    description:
+      "In this video we'll discuss how to use Guides for aligning and measuring objects on the canvas.",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "cAkOH9uaXHA",
+    image: "http://i3.ytimg.com/vi/cAkOH9uaXHA/maxresdefault.jpg",
+    name: "Figma Tutorial: Easing Curves",
+    description:
+      "In this video we'll learn about easing curves and how to create your custom bezier easing curves.",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "y29Xwt9dET0",
+    image: "http://i3.ytimg.com/vi/y29Xwt9dET0/maxresdefault.jpg",
+    name: "Figma Tutorial: Variants",
+    description:
+      "Variants reduce the complexity of your design system, makes it easier to find components, and maps your design components more closely to code. In this video we'll teach you how to create and use variants and component sets",
+    channelName: "Figma",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwngedleQSjBTUabDr5IuXLEaMLy6lSCFMPzBi00V5w=s88-c-k-c0x00ffffff-no-rj",
+    topic: "Figma Basics",
+  },
+
+  {
+    id: "YqQx75OPRa0",
+    image: "http://i3.ytimg.com/vi/YqQx75OPRa0/maxresdefault.jpg",
+    name: "Beginning Graphic Design: Fundamentals",
+    description:
+      "In this video, you’ll learn the fundamentals of graphic design.",
+    channelName: "GCFLearnFree.org",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwnhSc4rUizQ3LSxth3FxWehHQb8da2qs54dH5F9oIQ=s48-c-k-c0x00ffffff-no-rj",
+    topic: "Design Fundamentals",
+  },
+
+  {
+    id: "sByzHoiYFX0",
+    image: "http://i3.ytimg.com/vi/sByzHoiYFX0/maxresdefault.jpg",
+    name: "Beginning Graphic Design: Typography",
+    description:
+      "In this video, learn more about the basics of working with typography.",
+    channelName: "GCFLearnFree.org",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwnhSc4rUizQ3LSxth3FxWehHQb8da2qs54dH5F9oIQ=s48-c-k-c0x00ffffff-no-rj",
+    topic: "Design Fundamentals",
+  },
+
+  {
+    id: "_2LLXnUdUIc",
+    image: "http://i3.ytimg.com/vi/_2LLXnUdUIc/maxresdefault.jpg",
+    name: "Beginning Graphic Design: Color",
+    description:
+      "In this video, you’ll learn the basics of using color in graphic design.",
+    channelName: "GCFLearnFree.org",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwnhSc4rUizQ3LSxth3FxWehHQb8da2qs54dH5F9oIQ=s48-c-k-c0x00ffffff-no-rj",
+    topic: "Design Fundamentals",
+  },
+
+  {
+    id: "a5KYlHNKQB8",
+    image: "http://i3.ytimg.com/vi/a5KYlHNKQB8/maxresdefault.jpg",
+    name: "Beginning Graphic Design: Layout & Composition",
+    description:
+      "In this video, you’ll learn the basics of layout and composition in graphic design.",
+    channelName: "GCFLearnFree.org",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwnhSc4rUizQ3LSxth3FxWehHQb8da2qs54dH5F9oIQ=s48-c-k-c0x00ffffff-no-rj",
+    topic: "Design Fundamentals",
+  },
+
+  {
+    id: "MELKuexR3sQ",
+    image: "http://i3.ytimg.com/vi/MELKuexR3sQ/maxresdefault.jpg",
+    name: "Beginning Graphic Design: Images",
+    description:
+      "In this video, you’ll learn the basics of using images in graphic design.",
+    channelName: "GCFLearnFree.org",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwnhSc4rUizQ3LSxth3FxWehHQb8da2qs54dH5F9oIQ=s48-c-k-c0x00ffffff-no-rj",
+    topic: "Design Fundamentals",
+  },
+
+  {
+    id: "l-S2Y3SF3mM",
+    image: "http://i3.ytimg.com/vi/l-S2Y3SF3mM/maxresdefault.jpg",
+    name: "Beginning Graphic Design: Branding & Identity",
+    description:
+      "In this video, you’ll learn the basics of using branding and identity in graphic design.",
+    channelName: "GCFLearnFree.org",
+    channelLogo:
+      "https://yt3.ggpht.com/ytc/AAUvwnhSc4rUizQ3LSxth3FxWehHQb8da2qs54dH5F9oIQ=s48-c-k-c0x00ffffff-no-rj",
+    topic: "Design Fundamentals",
+  },
+];
+
+const topics = [
+  {
+    name: "Design Fundamentals",
+    image:
+      "https://images.unsplash.com/photo-1541462608143-67571c6738dd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    videos: 10,
+    totalLength: 5,
+  },
+  {
+    name: "Figma Basics",
+    image: "https://miro.medium.com/max/3200/1*FP8tGPBH3aRPxlH2XUX2pA.png",
+    videos: 10,
+    totalLength: 5,
+  },
+  {
+    name: "Color Theory",
+    image:
+      "https://images.unsplash.com/photo-1561070791-2526d30994b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80",
+    videos: 10,
+    totalLength: 5,
+  },
+  {
+    name: "Material Design",
+    image:
+      "https://lh3.googleusercontent.com/EWZvifAYMFXxRwxWa1GjXt8NrDPY39ndSav5oXHs91H5h2gPWi_lPxf3yKNXhZQCXoTGIav6nsu11iOIZ5aFSVW8MZkbPOCX5Eip=w760-h380",
+    videos: 10,
+    totalLength: 5,
+  },
+];
+
+const playlist = [
+  {
+    id: uuidv4(),
+    name: "Watch later",
+    videos: [],
+  },
+
+  {
+    id: uuidv4(),
+    name: "Demo Playlist",
+    videos: [],
+  },
+];
+export { videoLib, topics, playlist };
