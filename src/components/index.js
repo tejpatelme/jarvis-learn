5459fc3e889d001f10fb997fe1fdeb78e0df01b3
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-04-16 17:53:09 +0530

    add : re-exports file for components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as NavBar } from "./NavBar/NavBar";
+export { default as TopicCard } from "./TopicCard/TopicCard";
+export { default as VideoCard } from "./VideoCard/VideoCard";
+export { default as ModalBg } from "./ModalBg/ModalBg";
+export { default as PlaylistModal } from "./PlaylistModal/PlaylistModal";
+export { default as Toast } from "./Toast/Toast";
+export { default as ToastContainer } from "./Toast/ToastContainer";
