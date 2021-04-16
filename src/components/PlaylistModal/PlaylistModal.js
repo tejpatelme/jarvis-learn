d622adc5a1db805317308760f8a3a4c0042f0f83
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-04-16 17:54:07 +0530

    feat : Add to playlist

---
+++ src/components/PlaylistModal/PlaylistModal.js
@@ -0,0 +1,101 @@
+import "./PlaylistModal.css";
+import { useState } from "react";
+import { useUserData } from "../../context/userdata-context";
+import { useToast } from "../../context/toast-context";
+
+export default function PlaylistModal({ currentVideo }) {
+  const { playlist, dispatch } = useUserData();
+  const [input, setInput] = useState("");
+  const { dispatch: toastDispatch } = useToast();
+
+  const addNewPlaylist = () => {
+    if (input === "" || input.trim() === "") {
+      toastDispatch({
+        TYPE: "ERROR",
+        PAYLOAD: {
+          message: "Playlist name cannot be empty",
+        },
+      });
+      setInput("");
+      return;
+    }
+
+    dispatch({ TYPE: "ADD_NEW_PLAYLIST", PAYLOAD: input });
+  };
+
+  const handlePlaylist = (currentPlaylist, currentVideo) => {
+    const match = currentPlaylist.videos.find(
+      (video) => video.id === currentVideo.id
+    );
+    if (match) {
+      dispatch({
+        TYPE: "REMOVE_VIDEO_FROM_PLAYLIST",
+        PAYLOAD: {
+          playlistId: currentPlaylist.id,
+          videoId: currentVideo.id,
+        },
+      });
+      toastDispatch({
+        TYPE: "ERROR",
+        PAYLOAD: {
+          message: "Removed from playlist",
+        },
+      });
+      return;
+    }
+
+    dispatch({
+      TYPE: "ADD_VIDEO_TO_PLAYLIST",
+      PAYLOAD: {
+        playlistId: currentPlaylist.id,
+        video: currentVideo,
+      },
+    });
+    toastDispatch({
+      TYPE: "SUCCESS",
+      PAYLOAD: {
+        message: "Added to playlist",
+      },
+    });
+  };
+  return (
+    <div onClick={(e) => e.stopPropagation()} className="playlist-modal">
+      <ul>
+        {playlist.map((currentPlaylist, idx) => {
+          const match = currentPlaylist.videos.find(
+            (video) => video.id === currentVideo.id
+          );
+          return (
+            <li key={idx}>
+              <label>
+                <input
+                  checked={match}
+                  onChange={() => handlePlaylist(currentPlaylist, currentVideo)}
+                  type="checkbox"
+                  className="mr-2"
+                />
+                {currentPlaylist.name}
+              </label>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="input-container">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          placeholder="Create a new playlist"
+        />
+        <button onClick={() => addNewPlaylist()}>
+          <svg width="1.5rem" height="1.5rem" viewBox="0 0 24 24">
+            <path
+              d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Video/Video.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { useParams } from "react-router";
+import { videoLib } from "../../data/data";
+import "./Video.css";
+import { useUserData } from "../../context/userdata-context";
+import { ModalBg, PlaylistModal } from "../../components";
+
+export default function Video() {
+  const { videoId } = useParams();
+  const { liked, dispatch } = useUserData();
+  const [showModal, setShowModal] = useState(false);
+
+  const currentVideo = videoLib.find((video) => video.id === videoId);
+
+  const handleLike = (id) => {
+    const match = liked.find((video) => video.id === id);
+    if (match) {
+      dispatch({ TYPE: "REMOVE_FROM_LIKED", PAYLOAD: id });
+      return;
+    }
+
+    dispatch({ TYPE: "ADD_TO_LIKED", PAYLOAD: currentVideo });
+  };
+
+  return (
+    <div>
+      <div className="video-player-container">
+        <iframe
+          className="youtube-player"
+          width="100%"
+          height="100%"
+          src={`https://www.youtube.com/embed/${videoId}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <div className="video-details">
+        <p className="video-title">{currentVideo.name}</p>
+        <p className="video-description">{currentVideo.description}</p>
+        <div className="extra-details mb-5">
+          <div className="channel-details">
+            <div className="avatar avatar-lg mr-2">
+              <img src={currentVideo.channelLogo} className="img-round" />
+            </div>
+            <span className="channel-name">{currentVideo.channelName}</span>
+          </div>
+          <div>
+            <button
+              className="mr-3"
+              onClick={() => handleLike(currentVideo.id)}
+            >
+              {liked.find((video) => video.id === currentVideo.id) ? (
+                <span className="material-icons">favorite</span>
+              ) : (
+                <span className="material-icons">favorite_border</span>
+              )}
+            </button>
+            <button onClick={() => setShowModal((showModal) => !showModal)}>
+              <span className="material-icons">playlist_add</span>
+            </button>
+          </div>
+        </div>
+      </div>
+      {showModal && (
+        <ModalBg setShowModal={setShowModal}>
+          <PlaylistModal currentVideo={currentVideo} />
+        </ModalBg>
+      )}
+    </div>
+  );
+}
