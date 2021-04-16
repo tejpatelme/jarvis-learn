57f30fa9cefcf42b7582c990f03dd7709e83f1d2
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-04-16 17:52:56 +0530

    add : re-exports file for pages

---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Video } from "./Video/Video";
+export { default as Home } from "./Home/Home";
+export { default as Library } from "./Library/Library";
+export { default as Topic } from "./Topic/Topic";
