f3e71f45236003cb42074f2537fddaaaa8fcc791
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-04-16 17:55:33 +0530

    create : Routes for routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { NavBar, ToastContainer } from "./components";
+import { Home, Video, Library, Topic } from "./pages";
 
 function App() {
   return (
     <div className="App">
-      <h1>React app</h1>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/video/:videoId" element={<Video />} />
+        <Route path="/topic/:topicName" element={<Topic />} />
+        <Route path="/library" element={<Library />} />
+      </Routes>
+      <ToastContainer />
     </div>
   );
 }
