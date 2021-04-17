bf5aa9269e02aed27075f36e9ff007dcc7a371b6
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-04-17 09:10:58 +0530

    refactor : cleaned up imports

--- src/context/userdata-context.js
+++ src/context/userdata-context.js
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useReducer } from "react";
-import { playlist as initialPlaylist, playlist } from "../data/data";
+import { playlist as initialPlaylist } from "../data/data";
 import { v4 as uuidv4 } from "uuid";
 
 const data = {
