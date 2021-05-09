87a1e9742fb1e07f4336fab6504dbf571d70559e
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-09 14:07:37 +0530

    feat : Explore videos

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,6 +3,26 @@ import { videoLib, topics } from "../../data/data";
 import "./Home.css";
 
 export default function Home() {
+  const figmaVideos = videoLib
+    .filter((video) => video.topic === "Figma Basics")
+    .slice(0, 3);
+  const fundamentalsVideos = videoLib
+    .filter((video) => video.topic === "Design Fundamentals")
+    .slice(0, 3);
+  const materialVideos = videoLib
+    .filter((video) => video.topic === "Material Design")
+    .slice(0, 3);
+  const colorVideos = videoLib
+    .filter((video) => video.topic === "Color Theory")
+    .slice(0, 3);
+
+  const explore = [
+    ...figmaVideos,
+    ...fundamentalsVideos,
+    ...materialVideos,
+    ...colorVideos,
+  ];
+
   return (
     <div>
       <h2 className="mt-5 mb-2 pl-3">Topics</h2>
@@ -14,7 +34,7 @@ export default function Home() {
 
       <h2 className="mt-5 mb-2 pl-3">Explore </h2>
       <div className="video-container">
-        {videoLib.map((video, idx) => (
+        {explore.map((video, idx) => (
           <VideoCard key={idx} videoDetails={video} />
         ))}
       </div>
