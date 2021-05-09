a90541d663a0608f0b069b41d9e6e1292dadf184
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-09 13:53:50 +0530

    add : Re-export for Liked, Playlist page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,5 @@ export { default as Video } from "./Video/Video";
 export { default as Home } from "./Home/Home";
 export { default as Library } from "./Library/Library";
 export { default as Topic } from "./Topic/Topic";
+export { default as Playlist } from "./Playlist/Playlist";
+export { default as Liked } from "./Liked/Liked";
