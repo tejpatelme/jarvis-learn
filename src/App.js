6c0667ba86451f76fa9b0cc746f3f78a4b66737a
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-09 13:49:19 +0530

    add : Playlist, Liked videos routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { NavBar, ToastContainer } from "./components";
-import { Home, Video, Library, Topic } from "./pages";
+import { Home, Video, Library, Topic, Playlist, Liked } from "./pages";
 
 function App() {
   return (
@@ -12,6 +12,8 @@ function App() {
         <Route path="/video/:videoId" element={<Video />} />
         <Route path="/topic/:topicName" element={<Topic />} />
         <Route path="/library" element={<Library />} />
+        <Route path="/playlist/:playlistName" element={<Playlist />} />
+        <Route path="liked" element={<Liked />} />
       </Routes>
       <ToastContainer />
     </div>
