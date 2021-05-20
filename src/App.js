eceb1ede055be5d070b515013340d884e2bb9612
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-20 18:56:43 +0530

    add : data fetching from backend on page load

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import "./App.css";
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { NavBar, ToastContainer } from "./components";
 import { Home, Video, Library, Topic, Playlist, Liked } from "./pages";
+import API from "./api/api-urls";
+import axios from "axios";
+import { useUserData } from "./context/userdata-context";
 
 function App() {
+  const { dispatch } = useUserData();
+
+  const fetchVideos = async () => {
+    try {
+      const videos = await axios.get(API.FETCH_VIDEOS_URL);
+      const videoLib = videos.data.map((video) => {
+        const { _id: id, ...rest } = video;
+        return {
+          id,
+          ...rest,
+        };
+      });
+
+      dispatch({ TYPE: "SET_VIDEOS", PAYLOAD: { videos: videoLib } });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchVideos();
+  }, []);
+
   return (
     <div className="App">
       <NavBar />
