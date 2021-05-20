230e11bdffaf2d4bd6dde43050b811858869a4e4
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-20 18:55:00 +0530

    refactor : change variable name to match fetched data

--- src/components/VideoCard/VideoCard.js
+++ src/components/VideoCard/VideoCard.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 import "./VideoCard.css";
 
 export default function VideoCard({ videoDetails }) {
-  const { id, image, name, channelName, channelLogo } = videoDetails;
+  const { id, imageURL, name, channelName, channelLogoURL } = videoDetails;
   return (
     <Link to={`/video/${id}`}>
       <div className="video-card relative">
         <div>
           <img
             className="img-res video__image"
-            src={image}
+            src={imageURL}
             alt="video-thumbnail"
           />
           {/* <span className="badge badge-text">4:52</span> */}
@@ -19,7 +19,7 @@ export default function VideoCard({ videoDetails }) {
           <span>By</span>
           <img
             className="channel__image"
-            src={channelLogo}
+            src={channelLogoURL}
             alt="channel-logo"
           />
           {channelName}
