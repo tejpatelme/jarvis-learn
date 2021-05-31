a05a5e7259f740db799ad8e917eb5e802fa94b0b
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-31 12:54:50 +0530

    provide auth context to app

--- src/index.js
+++ src/index.js
@@ -4,16 +4,19 @@ import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import UserDataProvider from "./context/userdata-context";
 import ToastProvider from "./context/toast-context";
+import AuthProvider from "./context/auth-context";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <UserDataProvider>
-        <ToastProvider>
-          <App />
-        </ToastProvider>
-      </UserDataProvider>
-    </Router>
+    <UserDataProvider>
+      <ToastProvider>
+        <Router>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </Router>
+      </ToastProvider>
+    </UserDataProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
