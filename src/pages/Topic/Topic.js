05b1edf105393bce0a3fc5056455443075919cb9
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-31 12:52:22 +0530

    refactor : using data from context

--- src/pages/Topic/Topic.js
+++ src/pages/Topic/Topic.js
@@ -1,14 +1,15 @@
 import { VideoCard } from "../../components";
-import { videoLib } from "../../data/data";
 import { useParams } from "react-router";
+import { useUserData } from "../../context/userdata-context";
 
 export default function Topic() {
   const { topicName } = useParams();
+  const { videos } = useUserData();
   return (
     <>
       <h2 className="mt-5 mb-2 pl-3">{topicName}</h2>
       <div className="video-container">
-        {videoLib
+        {videos
           .filter((videos) => videos.topic === topicName)
           .map((video, idx) => (
             <VideoCard key={idx} videoDetails={video} />
--- src/pages/Video/Video.js
+++ src/pages/Video/Video.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useParams } from "react-router";
-import { videoLib } from "../../data/data";
+// import { videoLib } from "../../data/data";
 import "./Video.css";
 import { useUserData } from "../../context/userdata-context";
 import { ModalBg, PlaylistModal } from "../../components";
@@ -9,8 +9,10 @@ export default function Video() {
   const { videoId } = useParams();
   const { liked, dispatch } = useUserData();
   const [showModal, setShowModal] = useState(false);
+  const { videos } = useUserData();
+  const currentVideo = videos.find((video) => video.videoId === videoId);
 
-  const currentVideo = videoLib.find((video) => video.id === videoId);
+  const dataLoaded = videos.length === 0 ? false : true;
 
   const handleLike = (id) => {
     const match = liked.find((video) => video.id === id);
@@ -23,55 +25,61 @@ export default function Video() {
   };
 
   return (
-    <div>
-      <div className="video-player-container">
-        <iframe
-          className="youtube-player"
-          width="100%"
-          height="100%"
-          src={`https://www.youtube.com/embed/${videoId}`}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-      <div className="video-details">
-        <p className="video-title">{currentVideo.name}</p>
-        <p className="video-description">{currentVideo.description}</p>
-        <div className="extra-details mb-5">
-          <div className="channel-details">
-            <div className="avatar avatar-lg mr-2">
-              <img
-                src={currentVideo.channelLogo}
-                className="img-round"
-                alt="channel-logo"
-              />
+    <>
+      {dataLoaded ? (
+        <div>
+          <div className="video-player-container">
+            <iframe
+              className="youtube-player"
+              width="100%"
+              height="100%"
+              src={`https://www.youtube.com/embed/${videoId}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div className="video-details">
+            <p className="video-title">{currentVideo.name}</p>
+            <p className="video-description">{currentVideo.description}</p>
+            <div className="extra-details mb-5">
+              <div className="channel-details">
+                <div className="avatar avatar-lg mr-2">
+                  <img
+                    src={currentVideo.channelLogoURL}
+                    className="img-round"
+                    alt="channel-logo"
+                  />
+                </div>
+                <span className="channel-name">{currentVideo.channelName}</span>
+              </div>
+              <div>
+                <button
+                  className="mr-3"
+                  onClick={() => handleLike(currentVideo.id)}
+                >
+                  {liked.find((video) => video.id === currentVideo.id) ? (
+                    <span className="material-icons">favorite</span>
+                  ) : (
+                    <span className="material-icons">favorite_border</span>
+                  )}
+                </button>
+                <button onClick={() => setShowModal((showModal) => !showModal)}>
+                  <span className="material-icons">playlist_add</span>
+                </button>
+              </div>
             </div>
-            <span className="channel-name">{currentVideo.channelName}</span>
           </div>
-          <div>
-            <button
-              className="mr-3"
-              onClick={() => handleLike(currentVideo.id)}
-            >
-              {liked.find((video) => video.id === currentVideo.id) ? (
-                <span className="material-icons">favorite</span>
-              ) : (
-                <span className="material-icons">favorite_border</span>
-              )}
-            </button>
-            <button onClick={() => setShowModal((showModal) => !showModal)}>
-              <span className="material-icons">playlist_add</span>
-            </button>
-          </div>
+          {showModal && (
+            <ModalBg setShowModal={setShowModal}>
+              <PlaylistModal currentVideo={currentVideo} />
+            </ModalBg>
+          )}
         </div>
-      </div>
-      {showModal && (
-        <ModalBg setShowModal={setShowModal}>
-          <PlaylistModal currentVideo={currentVideo} />
-        </ModalBg>
+      ) : (
+        <p>Loading</p>
       )}
-    </div>
+    </>
   );
 }
