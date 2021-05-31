a1b41123f13df75e69d2a517f328fe59982ded13
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-31 12:54:24 +0530

    fetch playlists data on login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useCallback } from "react";
 import { Routes, Route } from "react-router-dom";
 import { NavBar, ToastContainer } from "./components";
-import { Home, Video, Library, Topic, Playlist, Liked } from "./pages";
+import {
+  Home,
+  Video,
+  Library,
+  Topic,
+  Playlist,
+  Liked,
+  Login,
+  Signup,
+} from "./pages";
 import API from "./api/api-urls";
 import axios from "axios";
 import { useUserData } from "./context/userdata-context";
+import { useAuth } from "./context/auth-context";
 
 function App() {
   const { dispatch } = useUserData();
+  const { isLoggedIn } = useAuth();
 
-  const fetchVideos = async () => {
+  const fetchVideos = useCallback(async () => {
     try {
       const videos = await axios.get(API.FETCH_VIDEOS_URL);
       const videoLib = videos.data.map((video) => {
@@ -25,11 +36,23 @@ function App() {
     } catch (err) {
       console.log(err);
     }
-  };
+  }, [dispatch]);
 
   useEffect(() => {
     fetchVideos();
-  }, []);
+  }, [fetchVideos]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(`${API.BASE_URL}/playlists`);
+
+        dispatch({ type: "SET_PLAYLISTS", payload: { playlists: data } });
+      } catch (err) {
+        console.log(err.response);
+      }
+    })();
+  }, [dispatch, isLoggedIn]);
 
   return (
     <div className="App">
@@ -41,6 +64,8 @@ function App() {
         <Route path="/library" element={<Library />} />
         <Route path="/playlist/:playlistName" element={<Playlist />} />
         <Route path="liked" element={<Liked />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
       <ToastContainer />
     </div>
