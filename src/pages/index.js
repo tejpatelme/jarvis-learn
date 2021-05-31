dabd7dc2c9235873d769468ec855ef4d8efb153d
Tej Patel <74844819+tejpatelme@users.noreply.github.com>
2021-05-31 12:53:56 +0530

    add: : login , signup pages to re-export

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,5 @@ export { default as Library } from "./Library/Library";
 export { default as Topic } from "./Topic/Topic";
 export { default as Playlist } from "./Playlist/Playlist";
 export { default as Liked } from "./Liked/Liked";
+export { default as Login } from "./Login/Login";
+export { default as Signup } from "./Signup/Signup";
